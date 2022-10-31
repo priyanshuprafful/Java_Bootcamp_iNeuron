2ec3a17e027e0abf2e03e8f9798b56979a93f4bb
priyanshuprafful <priyanshuprafful@gmail.com>
2022-10-31 20:40:30 +0530

    Changes made in

--- Demo_For_GIT.java
+++ Demo_For_GIT.java
@@ -2,6 +2,8 @@ class Demo{
 	public static void public static void main(String[] args) {
 		System.out.println("Hello it's a Demo");
 		System.out.println("Hello it's a Demo 1");
+		System.out.println("Changing in GitCloneDemo folder");
+
 
 
 	}
